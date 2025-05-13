232608f80a7513a6635b0b722dd16be33bb116e6
jefferson <jeffersoonaraujoo@gmail.com>
2025-05-13 14:37:06 -0300

    adicionando subido de ec2

---
+++ modules/ec2/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "vm_sg" {
+  name        = "vm-security-group"
+  description = "Permite SSH e acesso HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Altere para seu IP específico em produção!
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "vm" {
+  ami             = var.ami_id
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.vm_sg.name]
+
+  tags = {
+    Name = "TerraformVM"
+  }
+}
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "Região da AWS"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "Tipo da instância EC2"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID (Amazon Linux 2 em us-east-1)"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0"
+}
